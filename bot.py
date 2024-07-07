66a4e997a9cf73a45d9abd61ebb249d53595c004
MayoneJY <mayone6063@kakao.com>
2024-07-07 22:21:47 +0900

    test

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ from dotenv import load_dotenv
 from utils import locale_v2
 from utils.valorant.cache import get_cache
 
+
 load_dotenv()
 
 initial_extensions = ['cogs.admin', 'cogs.errors', 'cogs.notify', 'cogs.valorant']
